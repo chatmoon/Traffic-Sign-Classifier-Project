bce7d1518a11f54109a1ab730b7da0958790b2d9
mo <chatmoon@gmail.com>
2017-03-13 13:41:59 +0100

    [step1.2] done = class dataVisu{ def ( showBbox ) }
    Note: showBbox() shows a sampling of the dataset with the bounding box around the sign in the image
    
    Next: [step2.1] class dataPPro{ def ( proCrop ) }
          proCrop() crops each image using the bounding box around the sign from the dataset
          and resize it to 32x32
    
    .

--- _2_WIP/_JNBK_/_Coding_/P1703112110_dataPpro.py
+++
@@ -1,5 +0,0 @@
-class dataPPro():
-    def __init__(self, dataImg, dataLabel, path):
-        self.dataImg   = dataImg
-        self.dataLabel = dataLabel
-        self.path      = path
---
+++ _2_WIP/_JNBK_/_Coding_/_zArchive_/P1703112110_dataPpro.py
@@ -0,0 +1,5 @@
+class dataPPro():
+    def __init__(self, dataImg, dataLabel, path):
+        self.dataImg   = dataImg
+        self.dataLabel = dataLabel
+        self.path      = path
